18822b58074aa3215522acb8fe48c48c6c5f3b7e
Maksimov, Nikolay <niko.maksimov@x5.ru>
2022-05-27 17:47:36 +0300

    Fix

--- demo_code/assertion.py
+++ demo_code/assertion.py
@@ -1,18 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# developer : Nikolai Maksimov (X5 Group)
-# date: 17.11.2021
-
-
 def check_http_response(transaction, response):
-    response_body = str(response.content)
+    response_body = response.text
     if transaction == 'login':
         if 'Token' not in response_body:
             response.failure('Login failed')
 
-    if transaction == 'send_http':
-        if 'False' in response_body:
-            response.failure('Bad response')
-
-
+    if transaction == 'add_to_cart':
+        if 'добавлен в корзину' not in response_body:
+            response.failure(response_body)
