b24248bb9304966eb6874ea570d8af1e39246c8a
Maksimov, Nikolay <niko.maksimov@x5.ru>
2022-05-29 12:24:36 +0300

    [HB_2022] Refactoring

--- docker/locust_mock.py
+++ docker/locust_mock.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
+# Rebuild docker -> docker-compose up -d --no-deps --build mock
 
-# developer : Nikolai Maksimov (X5 Group)
-# date: 17.11.2021
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import base64
 import random
 import time
+import json
+
 
 app = FastAPI()
 
 
-@app.get("/demo_http")
-def demo_http():
-    time.sleep(random.randrange(1, 3))
-    return random.choice([f'Response is {True}',
-                          f'Response is {False}', ])
+@app.post("/cart/add")
+async def add_to_cart(request: Request):
+    product = json.loads(await request.body())
+    prod_name = product['Product']
+    prod_code = product['Prod_code']
+    answers = [
+        f"Товар #{prod_code} - '{prod_name}' добавлен в корзину",
+        "Ошибка добавления товара",
+        ]
+    return random.choice(answers)
 
 
 @app.get("/login/{username}")
